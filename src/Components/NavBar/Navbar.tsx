d264ceabac26b152fb868eb5a7315e1a6f8b379e
KarolisKazlauskas <106345633+KarolisKazlauskas@users.noreply.github.com>
2022-10-01 11:19:47 +0300

    Update Navbar.tsx

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,27 +1,20 @@
 import React from 'react'
 
-
-
 export default function Navbar() {
 
   return (
-    
-    
-    <div>
-        <nav className="navbar bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#/">Home</a>
-          </div>
-        </nav>
 
-        {/* <!-- As a heading --> */}
-        <nav className="navbar bg-light">
-          <div className="container-fluid">
-            <span className="navbar-brand mb-0 h1">People</span>
-          </div>
-        </nav>
-    </div>
-   
-  )
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/Home">Home</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="/person">Person <span className="sr-only">(current)</span></a>
+        </div>
+      </div>
+    </nav>
+
+  );
 }
-
