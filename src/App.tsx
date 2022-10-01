2b697a057605b5ff33c0bb72d24c5359e5a80f52
KarolisKazlauskas <106345633+KarolisKazlauskas@users.noreply.github.com>
2022-10-01 11:09:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import People from './Components/People';
 import './Components/NavBar/Navbar';
-import './App.css';
 import Navbar from './Components/NavBar/Navbar';
-import Cards from './Components/Cards';
 
 function App() {
   return (
